b20c0dbd0f3701c0e6c93625e571b5b77438aabc
Manuvai <44281637+manuvai@users.noreply.github.com>
2024-05-30 20:09:22 +0200

    feat: Permet la génération automatique des routes API et dto

---
+++ src/main/java/fr/manuvai/tereroa/controllers/UserController.java
@@ -0,0 +1,8 @@
+package fr.manuvai.tereroa.controllers;
+
+import fr.manuvai.tereroa.api.UsersApi;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController implements UsersApi {
+}
