d03ef76480288e335ee695a9d522ca26d31d6c61
Manuvai <44281637+manuvai@users.noreply.github.com>
2024-06-05 19:38:13 +0200

    fix: Correction du statut de réponse

--- src/main/java/fr/manuvai/tereroa/exceptions/TripDatesIncorrectException.java
+++ src/main/java/fr/manuvai/tereroa/exceptions/TripDatesIncorrectException.java
@@ -1,5 +1,9 @@
 package fr.manuvai.tereroa.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class TripDatesIncorrectException extends RuntimeException {
     public TripDatesIncorrectException() {
         super("Les dates de votre voyage ne sont pas correctes");
